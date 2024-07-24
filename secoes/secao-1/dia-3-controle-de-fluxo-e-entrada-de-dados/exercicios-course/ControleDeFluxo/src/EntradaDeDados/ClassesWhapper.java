064bb5ec30f614f0ad8ce185bc5d764acd74434a
Rodrigo Christofani <christofanir@gmail.com>
2024-07-24 00:55:48 -0300

    Exercícios do course dia 3 concluídos.

---
+++ secoes/secao-1/dia-3-controle-de-fluxo-e-entrada-de-dados/exercicios-course/ControleDeFluxo/src/EntradaDeDados/ClassesWhapper.java
@@ -0,0 +1,11 @@
+package EntradaDeDados;
+
+public class ClassesWhapper {
+    public static  void main(String[] args) {
+        Integer valor = 42; // autoboxing]
+
+        System.out.println(
+           Integer.toBinaryString(valor)
+        );
+    }
+}
