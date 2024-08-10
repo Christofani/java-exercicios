89c988c458b752723f753d32ccb0225741c909b4
Rodrigo Christofani <christofanir@gmail.com>
2024-08-10 10:53:21 -0300

    interface...

--- secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/Application.java
+++ secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/Application.java
@@ -4,6 +4,19 @@ public class Application {
 
   public static void main(String[] args) {
     Laptop laptop = new Laptop("Samsung", 100, 14);
-    laptop.bootUp();
+    PowerBank powerBank = new PowerBank();
+
+    plugDevice(laptop);
+    plugDevice(powerBank);
+  }
+
+//  public static void checkDevice(ComputerDevice device) {
+//    System.out.println("Verificando dispositivo de marca " + device.getBrand());
+//    device.bootUp();
+//  }
+
+  public static void plugDevice(Chargeable chargeable) {
+    System.out.println("A bateria atual é " + chargeable.getBatteryLevel());
+    chargeable.charge();
   }
 }
--- secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/Laptop.java
+++ secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/Laptop.java
@@ -1,9 +1,10 @@
 package com.betrybe.tech;
 
-public class Laptop extends  ComputerDevice {
-  private  double size;
+public class Laptop extends ComputerDevice implements Chargeable {
 
-  public  Laptop(String brand, double storageCapacity, double size) {
+  private double size;
+
+  public Laptop(String brand, double storageCapacity, double size) {
     super(brand, storageCapacity);
 
     this.size = size;
@@ -12,5 +13,15 @@ public class Laptop extends  ComputerDevice {
   @Override
   public void bootUp() {
     System.out.println("Inicializando o laptop de tamanho " + size + "...");
+  }
+
+  @Override
+  public void charge() {
+    System.out.println("Carregando o laptop...");
+  }
+
+  @Override
+  public int getBatteryLevel() {
+    return 85;
   }
 }
