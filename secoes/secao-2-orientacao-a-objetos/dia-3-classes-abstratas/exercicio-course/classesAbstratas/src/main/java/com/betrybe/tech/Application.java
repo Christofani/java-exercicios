812f552e714741a73bdd9b6b7b01d761f72efb0a
Rodrigo Christofani <christofanir@gmail.com>
2024-08-12 09:27:51 -0300

    Conteúdo course finalizado.

--- secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/Application.java
+++ secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/Application.java
@@ -3,20 +3,10 @@ package com.betrybe.tech;
 public class Application {
 
   public static void main(String[] args) {
-    Laptop laptop = new Laptop("Samsung", 100, 14);
-    PowerBank powerBank = new PowerBank();
+    ComputerDevice gerenericDevice = new Laptop("Apple", 256.0, 13.3);
 
-    plugDevice(laptop);
-    plugDevice(powerBank);
-  }
-
-//  public static void checkDevice(ComputerDevice device) {
-//    System.out.println("Verificando dispositivo de marca " + device.getBrand());
-//    device.bootUp();
-//  }
-
-  public static void plugDevice(Chargeable chargeable) {
-    System.out.println("A bateria atual é " + chargeable.getBatteryLevel());
-    chargeable.charge();
+    if (gerenericDevice instanceof Laptop) {
+      Laptop specificLaptop = (Laptop) gerenericDevice;
+    }
   }
 }
--- secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/ComputerDevice.java
+++ secoes/secao-2-orientacao-a-objetos/dia-3-classes-abstratas/exercicio-course/classesAbstratas/src/main/java/com/betrybe/tech/ComputerDevice.java
@@ -1,20 +1,26 @@
 package com.betrybe.tech;
 
 public abstract class ComputerDevice {
-  private  String brand;
-  private  double storageCapacity;
 
-  public  ComputerDevice(String brand, double storageCapacity) {
+  public static final double MAX_STORAGE_CAPACITY = 2048.0;
+  private final String brand;
+  private double storageCapacity;
+
+  public ComputerDevice(String brand, double storageCapacity) {
     this.brand = brand;
     this.storageCapacity = storageCapacity;
   }
 
-  public  String getBrand() {
-    return  brand;
+  public String getBrand() {
+    return brand;
   }
 
-  public  double getStorageCapacity() {
-    return  storageCapacity;
+  public double getStorageCapacity() {
+    return storageCapacity;
+  }
+
+  public final void shutDown() {
+    System.out.println("Desligando o dispositivo...");
   }
 
   public abstract void bootUp();
