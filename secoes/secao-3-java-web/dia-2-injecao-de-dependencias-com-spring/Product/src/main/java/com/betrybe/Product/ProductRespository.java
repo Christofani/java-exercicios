aa04eae7ee5530207afe0af93024aba820749580
Rodrigo Christofani <christofanir@gmail.com>
2024-08-12 16:31:29 -0300

    Injeção de dependência com Spring.

--- secoes/secao-3-java-web/dia-2-injecao-de-dependencias-com-spring/Product/src/main/java/com/betrybe/Product/ProductRespository.java
+++ secoes/secao-3-java-web/dia-2-injecao-de-dependencias-com-spring/Product/src/main/java/com/betrybe/Product/ProductRespository.java
@@ -1,9 +1,18 @@
 package com.betrybe.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRespository {
+
+  private Database database;
+
+  @Autowired
+  public ProductRespository(Database database) {
+    this.database = database;
+  }
+
   public String getMessage() {
     return "Mensagem: produto retornado";
   }
